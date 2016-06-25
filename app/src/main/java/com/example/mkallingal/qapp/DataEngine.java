5bd842acdca19503d70116d08b794410164113b4
KALLINGAL Muhammed Irshad <irshad@cardtrend.com>
2016-06-25 21:21:35 +0800

    remove unused code

--- app/src/main/java/com/example/mkallingal/qapp/DataEngine.java
+++ app/src/main/java/com/example/mkallingal/qapp/DataEngine.java
@@ -67,33 +67,6 @@ public class DataEngine {
     }
 
 
-    public void ClearCache() {
-        putValue("cardslist", "");
-        putValue("invoicelist", "");
-        putValue("AcctNo", "");
-        putValue("totalvolume", "");
-        putValue("txntrend", "");
-        putValue("topspender","");
-        putValue("fuelconsumption", "");
-        putValue("txnlist", "");
-    }
-
-    public  void ClearSignoutCache(){
-        ClearCache();
-        putValue("CorpCd","");
-        putValue("PrivilegeCd", "");
-        putValue("accountslist","");
-        putValue("corporateinfo","");
-        putValue("AcctInfo", "");
-        putValue("UserName", " ");
-        putValue("IsSuper", "");
-        putValue("UserName","");
-    }
-
-
-    private  void DisplaySnackBar(){
-
-    }
 
    public String GenerateUUID(){
        DateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
@@ -101,58 +74,6 @@ public class DataEngine {
         UUID x= UUID.randomUUID();
       String[] y= x.toString().split("-");
        return y[y.length-1]+sdt;
-    }
-
-
-    public String SaveImageToInternalStorage(Bitmap bitmapImage, String UUID)  {
-        ContextWrapper cw = new ContextWrapper(this._Context);
-        // path to /data/data/yourapp/app_data/imageDir
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        // Create imageDir
-        File mypath=new File(directory,UUID+".png");
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(mypath);
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 20, fos);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try{
-                fos.close();
-            }catch (Exception ex){
-                ex.printStackTrace();
-            }
-
-        }
-        return directory.getAbsolutePath();
-    }
-
-
-    public Bitmap LoadImageFromInternalStorage(String path, String UUID)
-    {
-        Bitmap b;
-        try {
-            File f=new File(path, UUID+".png");
-           // b = BitmapFactory.decodeStream(new FileInputStream(f));
-         //   return  b;
-
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 2;
-            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(f), null, options);
-
-           return  bitmap;
-
-            // ImageView IMG=(ImageView)context.(R.id.imgPicker);
-            // IMG.setImageBitmap(b);
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-            String x= e.getMessage();
-            return  BitmapFactory.decodeResource(_Context.getResources(),
-                    R.drawable.placeholder);
-        }
     }
 
 }
